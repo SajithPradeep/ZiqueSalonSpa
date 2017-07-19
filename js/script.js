1b8844eb6a92334257e4092931486befa7f20a0e
Pradeep <sapradeep@deloitte.com>
2017-07-19 18:53:03 +0530

    google-form-desktop-not responsive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 
 function whatsappToggle() {
     var displayType = document.getElementById("whatsapp").style.display;
-   if(displayType === "" || displayType === "none") {
+    if(displayType === "" || displayType === "none") {
        document.getElementById("whatsapp").style.display = "flex";
-   }
-   else {
+    }
+    else {
        document.getElementById("whatsapp").style.display = "none"
-   }
+    }
+}
+function triggerOverlay() {
+    var displayType = document.getElementById("google-form").style.display;
+    if(displayType === "" || displayType === "none") {
+       document.getElementById("google-form").style.display = "flex";
+    }
+    else {
+       document.getElementById("google-form").style.display = "none"
+    }
 }
