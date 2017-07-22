06f7c1d221440f3d0c89accf6d47764921341350
Pradeep <sapradeep@deloitte.com>
2017-07-22 22:54:35 +0530

    adding more values for services

--- js/script.js
+++ js/script.js
@@ -127,18 +127,133 @@ var data = {
                     {
                         name: "HAIRCUT ",
                         stylist: "Style Director",
-                        amount: 950
+                        amount: 1750
                     }, {
                         name: "HAIRCUT ",
                         stylist: "Top Stylist",
-                        amount: 650
+                        amount: 1250
                     },
                     {
                         name: "HAIRCUT ",
-                        stylist: "Top Stylist",
-                        amount: 650
+                        stylist: "Senior  Stylist",
+                        amount: 850
+                    },  {
+                        name: "Kids Haircut",
+                        stylist: "(Below 10 years)",
+                        amount: 450
+                    }, {
+                        name: "Hair Wash & Blast Dry ",
+                        stylist: "",
+                        amount: 550
+                    }, {
+                        name: "Blow Dry ",
+                        stylist: "",
+                        amount: "650 Onwards"
+                    }, {
+                        name: "Tongs",
+                        stylist: "",
+                        amount: "950 Onwards"
+                    },{
+                        name: "Ironing",
+                        stylist: "",
+                        amount: "850 Onwards"
+                    }, {
+                        name: "Up Styling",
+                        stylist: "",
+                        amount: 950
                     }
                 ]
+            }, {
+                 serviceType: "Colour Bar Services",
+                 services : [
+                     {  
+                        serviceSubType: "Absolute Colour",
+                        name: "Global Base Colour ",
+                        stylist: "",
+                        amount: "2550 Onwards" 
+                     },  {
+                        name: "Global Fashion Colour ",
+                        stylist: "",
+                        amount: "3550 Onwards" 
+                    }, {
+                        name: "Root Touch-up ",
+                        stylist: "",
+                        amount: "1550 Onwards" 
+                    }, {  
+                        serviceSubType: "Creative Colour",
+                        name: "Root Touch-up",
+                        stylist: "",
+                        amount: "1750 Onwards" 
+                     }, {
+                        name: "Colour Refresh",
+                        stylist: "",
+                        amount: "2050 Onwards" 
+                    },{
+                        name: "Balayage ",
+                        stylist: "",
+                        amount: "4550 Onwards" 
+                    }, {
+                        name: "Ombre ",
+                        stylist: "",
+                        amount: "4550 Onwards" 
+                    }, {
+                        name: "Full Head Highlights",
+                        stylist: "",
+                        amount: "5050 Onwards" 
+                    }, {
+                        name: "Single Streak ",
+                        stylist: "",
+                        amount: "450 Onwards" 
+                    }
+                 ]
+            }, {
+                serviceType: "Texture",
+                services: [
+                    {
+                        name: "Keratin Treatment",
+                        stylist: "",
+                        amount: "7050 Onwards" 
+                    }, {
+                        name: "Smoothening Treatment",
+                        stylist: "",
+                        amount: "6050 Onwards" 
+                    },{
+                        name: "Smoothening Fringe",
+                        stylist: "",
+                        amount: 2050 
+                    }]
+            }, {
+                serviceType: "Washhouse Services",
+                services: [
+                    {
+                        name: "Hair Wash & Deep Conditioning ",
+                        stylist: "",
+                        amount: 1350 
+                    }, {
+                        name: "Classic Hair Spa",
+                        stylist: "",
+                        amount: 1550 
+                    },{
+                        name: "Luxury Hair Spa",
+                        stylist: "",
+                        amount: 2050 
+                    }, {
+                        name: "Scalp Treatment ",
+                        stylist: "",
+                        amount: 2550 
+                    }, {
+                        name: "Hair Fall Treatment",
+                        stylist: "",
+                        amount: 2550 
+                    }, {
+                        name: "Deep Hair Repair Treatment ",
+                        stylist: "",
+                        amount: 2850 
+                    }, {
+                        name: "Head Oil Massage",
+                        stylist: "",
+                        amount: 750 
+                    }]
             }]
         }}
     ]
